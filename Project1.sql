58182fdd37a17b34e0dac88f4fec7e13f69423cb
[HopeRise] <evette.garay@gmail.com>
2024-03-02 21:38:13 -0600

    to make importing the data my easier

--- Project1.sql
+++ Project1.sql
@@ -10,3 +10,77 @@
 
 
 --stored porcedure;
+
+--creating tables;
+
+Create table Publication(
+    PublicationID INT,
+    Title VARCHAR(400),
+    Topic VARCHAR(100),
+    Date VARCHAR(20),
+    PublicationType VARCHAR(25),
+    PRIMARY KEY (PublicationID)
+);
+
+create table Conference(
+    ConferenceID int,
+    Name VARCHAR(400),
+    Topic VARCHAR(200),
+    Location VARCHAR(200),
+    StartDate VARCHAR(200),
+    EndDate VARCHAR(200),
+    EventFormat VARCHAR(200),
+    PRIMARY KEY (ConferenceID)
+);
+
+create table Authors(
+    AuthorsID int,
+    FirstName varchar(150),
+    LastName varchar(150),
+    Affiliation VARCHAR(300),
+    PublicationType VARCHAR(300),
+    PRIMARY key (AuthorsID)
+);
+
+create table JournalArticle(
+    AuthorsID int,
+    PublicationID int,
+    ArticleTitle varchar(300),
+    Volume int,
+    Issue int,
+    primary KEY (AuthorsID, PublicationID),
+    Foreign Key (AuthorsID) REFERENCES Authors(AuthorsID),
+    Foreign Key (PublicationID) REFERENCES Publication(PublicationID)
+);
+
+create table PodcastEpisode(
+    AuthorsID int,
+    PublicationID int,
+    Ep_name varchar(300),
+    Ep_num int,
+    Duration int,
+    primary KEY (AuthorsID, PublicationID),
+    Foreign Key (AuthorsID) REFERENCES Authors(AuthorsID),
+    Foreign Key (PublicationID) REFERENCES Publication(PublicationID)
+);
+
+
+create table Book(
+    AuthorsID int,
+    PublicationID int,
+    TotalPages int,
+    ChapterNumbers int,
+    Publisher varchar(400),
+    primary KEY (AuthorsID, PublicationID),
+    Foreign Key (AuthorsID) REFERENCES Authors(AuthorsID),
+    Foreign Key (PublicationID) REFERENCES Publication(PublicationID)
+);
+
+
+create table PublicationConference(
+    AuthorsID int,
+    PublicationID int,
+    primary KEY (AuthorsID, PublicationID),
+    Foreign Key (AuthorsID) REFERENCES Authors(AuthorsID)
+);
+
