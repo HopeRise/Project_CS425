c5fe20cb8c34e32e8adf970d6774997940be4942
[HopeRise] <evette.garay@gmail.com>
2024-04-01 15:18:24 -0500

    Create DatabaseApplication.java

---
+++ DatabaseApplication.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseApplication {
+
+    public static void main(String[] args) {
+        // Database URL
+        String url = "jdbc:mysql://127.0.0.1:3306/?user=root";
+
+        String username = "root";
+        String password = "Gilbert:0529";
+
+        Connection myConnection = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            myConnection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to the database!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC driver not found!");
+        } catch (SQLException e) {
+            System.out.println("SQLException : " + e);
+            System.out.println("Connection failed!");
+        } finally {
+            try {
+                if (myConnection != null && !myConnection.isClosed()) {
+                    myConnection.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("Error closing connection: " + e);
+            }
+        }
+    }
+}
