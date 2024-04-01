1a12e4016080cd85d0a13555a76c660c1dd00f20
[HopeRise] <evette.garay@gmail.com>
2024-04-01 15:00:13 -0500

    base

--- DatabaseApplication.java
+++ DatabaseApplication.java
@@ -2,29 +2,37 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DatabaseApplication{
+public class DatabaseApplication {
 
-    public static void main (String [] args){
-
+    public static void main(String[] args) {
         // Database URL
-        String url = "jdbc:mysql://localhost3306/hi";
+        String url = "jdbc:mysql://127.0.0.1:3306/?user=root";
 
-        String username = "username";
-        String password = "password";
-        
-        try{
+        String username = "root";
+        String password = "Gilbert:0529";
+
+        Connection myConnection = null;
+
+        try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection myConnection = DriverManager.getConnection(url, username, password);
-
-        } catch (ClassNotFoundException e){
-
+            myConnection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to the database!");
+        } catch (ClassNotFoundException e) {
             System.out.println("JDBC driver not found!");
             e.printStackTrace();
-
-        } catch (SQLException e){
-
+        } catch (SQLException e) {
+            System.out.println("SQLException : " + e);
             System.out.println("Connection failed!");
             e.printStackTrace();
+        } finally {
+            try {
+                if (myConnection != null && !myConnection.isClosed()) {
+                    myConnection.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("Error closing connection: " + e);
+                e.printStackTrace();
+            }
         }
     }
 }
